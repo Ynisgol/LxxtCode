fb1315014198ad78808ccbcdb9e28a6c5922bbe7
Ynisgol <ynisgol@gmail.com>
2015-11-02 21:59:09 -0600

    Sliding Window Maximum done.

---
+++ LintCode/Sliding Window Maximum.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+
+class Solution:
+    def __init__(self):
+        return
+
+    """
+    @param nums: A list of integers.
+    @return: The maximum number inside the window at each moving.
+    """
+    @staticmethod
+    def max_sliding_window(nums, k):
+        # write your code here
+        # we can try a max deque that stores index
+        # and a deque that stores the window
+        # 1. when the window deque is not full
+        #   put one into the window deque
+        #   do
+        #   (peek tail of max deque, if greater than the tail, pop then add)
+        #   until smaller than tail or deque empty
+        #   <deque head always stores current max>
+        # 2. when the window deque is full
+        #   do all the above, also the following
+        #   remove head, add tail
+        #   if the head index == the head of max deque (can't be >),
+        #       remove head of max deque
+        #   if <,
+        #       do nothing to max deque
+        # in each iteration, return the head of max deque
+        # until no more num to add to window deque
+        # Time Complexity:
+        # each num: add to window deque once, add to max deque once, pop once
+        #           peek once, remove once = O(n)
+        # max size of both deque = k
+        # ---------------------------------------------------------------------
+        if not nums or k == 0:
+            return []
+        else:  # nums contains at least 1 number and k > 0
+            swm = []
+            window_dq = deque()
+            max_dq = deque()
+            for i in range(len(nums)):
+                window_dq.append(i)
+                while max_dq and nums[i] > nums[max_dq[-1]]:
+                    max_dq.pop()
+                max_dq.append(i)
+                if len(window_dq) > k:
+                    if window_dq.popleft() == max_dq[0]:
+                        max_dq.popleft()
+                if i >= k - 1:
+                    swm += [nums[max_dq[0]]]
+            return swm
+
+print Solution().max_sliding_window([1, 2, 7, 7, 2, 10, 3, 4, 5], 2)
